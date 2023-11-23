edc9d5b1c8fa848de679dbb0d17453e89452565c
iojabal <iojabal9@gmail.com>
2023-11-23 00:37:25 -0400

    Register View

--- src/views/Register/Registrar.jsx
+++ src/views/Register/Registrar.jsx
@@ -30,6 +30,15 @@ const Registrar = () => {
         return password === confirmPassword;
     };
 
+    const doRegister = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:5000/api/')
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
